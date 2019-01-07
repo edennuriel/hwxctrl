65b74e0f4383012e1f9c2a42534f307aa1cf3c97
edennuriel <edennuriel@hotmail.com>
2019-01-07 22:14:36 +0000

    new bp

---
+++ cbcli/clusters/v2/cc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+source ~/hwxctrl/scripts/utils.sh
+source ~/hwxctrl/scripts/cb.sh
+source ~/hwxctrl/scripts/cbcli.sh
+if [[ -z $1 ]] 
+then 
+	source ../../bootstrap.sh
+	#cd ../../blueprints
+	cbadd_blueprint bp.json
+	cbadd_mpacks http://public-repo-1.hortonworks.com/HDF/centos7/3.x/updates/3.2.0.0/tars/hdf_ambari_mp/hdf-ambari-mpack-3.2.0.0-520.tar.gz hdf32
+	cbadd_mpacks http://public-repo-1.hortonworks.com/HDP-SOLR/hdp-solr-ambari-mp/solr-service-mpack-4.0.0.tar.gz solr7
+	~/hwxctrl/scripts/prepare_recpies.sh
+fi
+
+cb cluster create --cli-input-json cc.json
